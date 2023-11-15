57a36f64cd8f723d906481cbba7ede22dc8121f1
FXRFXN <101660475+FXRFXN@users.noreply.github.com>
2023-11-15 15:08:02 -0600

    Update global.php

--- app/config/global.php
+++ app/config/global.php
@@ -1,21 +1,21 @@
-<?php 
+<?php
 //ip de la pc servidor base de datos
-define("DB_HOST", "localhost");
-
+define("DB_HOST", "viaduct.proxy.rlwy.net");
+ 
 // nombre de la base de datos
-define("DB_NAME", "sistema");
-
-
+define("DB_NAME", "railway");
+ 
+ 
 //nombre de usuario de base de datos
 define("DB_USERNAME", "root");
-
+ 
 //conraseña del usuario de base de datos
-define("DB_PASSWORD", "");
-
+define("DB_PASSWORD", "5-B3gaCgCAeHach15gcFF5CAEcD3ddFg");
+ 
 //codificacion de caracteres
 define("DB_ENCODE", "utf8");
-
+ 
 //nombre del proyecto
 define("PRO_NOMBRE", "CompartiendoCodigos");
  
- ?> 
+ ?>
