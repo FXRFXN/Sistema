3eb0e3de8004c4f8fe6fded5c6ba58dd01165e50
FXRFXN <101660475+FXRFXN@users.noreply.github.com>
2023-11-15 15:53:22 -0600

    Update global.php

--- app/config/global.php
+++ app/config/global.php
@@ -1,21 +1,21 @@
-<?php
+<?php 
 //ip de la pc servidor base de datos
-define("DB_HOST", "viaduct.proxy.rlwy.net");
- 
+define("DB_HOST", "by7mu16zhz1zguk9nqso-mysql.services.clever-cloud.com");
+
 // nombre de la base de datos
-define("DB_NAME", "railway");
- 
- 
+define("DB_NAME", "by7mu16zhz1zguk9nqso");
+
+
 //nombre de usuario de base de datos
-define("DB_USERNAME", "root");
- 
+define("DB_USERNAME", "uquyebwfnkfaejgs");
+
 //conraseña del usuario de base de datos
-define("DB_PASSWORD", "5-B3gaCgCAeHach15gcFF5CAEcD3ddFg");
- 
+define("DB_PASSWORD", "C0H6Hq1YtpqhkUdy9lKf");
+
 //codificacion de caracteres
 define("DB_ENCODE", "utf8");
- 
+
 //nombre del proyecto
 define("PRO_NOMBRE", "CompartiendoCodigos");
  
- ?>
+ ?> 
