31832747f12b4674e6f9dc0de829d040e87775f7
FXRFXN <101660475+FXRFXN@users.noreply.github.com>
2023-11-21 11:59:12 -0600

    Update historial_tutoria_alumno.php

--- usuarios/historial_tutoria_alumno.php
+++ usuarios/historial_tutoria_alumno.php
@@ -72,14 +72,26 @@ if (isset($_GET['id'])) {
 
 ?>
     
-    <?php 
-   $sql = "SELECT  nombres,ap_paterno,ap_materno,numero_control,carrera,semestre FROM tb_usuarios WHERE numero_control = '{$numero_control}'";
-  $resultado = mysqli_query($conexion,$sql);
-  ?>
+    
 
 <?php 
-  $sqli= "SELECT semestre_cursado,estado_acreditacion,ciclo_escolar FROM cursadas where id_alumno = '{$numero_control}'";
-  $resultadoi = mysqli_query($conexion,$sqli);
+$sql = "SELECT  nombres,ap_paterno,ap_materno,numero_control,carrera,semestre FROM tb_usuarios WHERE numero_control = '{$numero_control}'";
+$resultado = mysqli_query($conexion,$sql);
+
+
+  if(!empty($_REQUEST["nume"])){ $_REQUEST["nume"] = $_REQUEST["nume"];}else{ $_REQUEST["nume"] = '1';}
+  if($_REQUEST["nume"] == "" ){$_REQUEST["nume"] = "1";}
+  $articulos=mysqli_query($conexion,"SELECT semestre_cursado,estado_acreditacion,ciclo_escolar FROM cursadas where id_alumno = '{$numero_control}'");
+  $num_registros=@mysqli_num_rows($articulos);
+  $registros= '5';
+  $pagina=$_REQUEST["nume"];
+  if (is_numeric($pagina))
+  $inicio= (($pagina-1)*$registros);
+  else
+  $inicio=0;
+  $busqueda=mysqli_query($conexion,"SELECT semestre_cursado,estado_acreditacion,ciclo_escolar FROM cursadas where id_alumno = '{$numero_control}'LIMIT $inicio,$registros;");
+  $paginas=ceil($num_registros/$registros);
+   
   ?>
     
       <?php include('../layout/menu.php'); ?>
@@ -145,7 +157,7 @@ if (isset($_GET['id'])) {
 
                                              
    <?php
-      while($filass = mysqli_fetch_assoc($resultadoi)){
+      while($filass = mysqli_fetch_assoc($busqueda)){
 ?>
      <tr>
        <td>
@@ -174,6 +186,38 @@ if (isset($_GET['id'])) {
      
     </table>
 </div>
+<!-- paginacion //////////////////////////////////////-->
+<div class="container-fluid col-12">
+    <ul class="pagination pg-dark d-flex justify-content-center align-items-center" style="float: none;">
+        <li class="page-item">
+            <?php
+            if ($_REQUEST["nume"] == "1") {
+                $_REQUEST["nume"] = "1";
+                echo  "";
+            } else {
+                if ($pagina > 1) {
+                    $ant = $_REQUEST["nume"] - 1;
+                    echo "<a class='page-link' aria-label='Previous' href='historial_tutoria_alumno.php?id=" . urlencode($_GET['id']) . "'><span aria-hidden='true'>&laquo;</span><span class='sr-only'>Previous</span></a>";
+                    echo "<li class='page-item'><a class='page-link' href='historial_tutoria_alumno.php?id=" . urlencode($_GET['id']) . "&nume=" . ($pagina - 1) . "'>" . $ant . "</a></li>";
+                }
+            }
+            echo "<li class='page-item active'><a class='page-link'>" . $_REQUEST["nume"] . "</a></li>";
+            $sigui = $_REQUEST["nume"] + 1;
+            $ultima = $num_registros / $registros;
+            if ($ultima == $_REQUEST["nume"] + 1) {
+                $ultima == "";
+            }
+            if ($pagina < $paginas && $paginas > 1) {
+                echo "<li class='page-item'><a class='page-link' href='historial_tutoria_alumno.php?id=" . urlencode($_GET['id']) . "&nume=" . ($pagina + 1) . "'>" . $sigui . "</a></li>";
+            }
+            if ($pagina < $paginas && $paginas > 1) {
+                echo "<li class='page-item'><a class='page-link' aria-label='Next' href='historial_tutoria_alumno.php?id=" . urlencode($_GET['id']) . "&nume=" . ceil($ultima) . "'><span aria-hidden='true'>&raquo;</span><span class='sr-only'>Next</span></a></li>";
+            }
+            ?>
+    </ul>
+</div>
+
+<!-- end paginacion ///////////////////////// -->
 </section>
 </div>
 
